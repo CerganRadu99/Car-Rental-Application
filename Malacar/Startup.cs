43ac755f8362632154543e2e4700af7838babff2
CiprianP99 <57409814+CiprianP99@users.noreply.github.com>
2021-04-11 19:28:48 +0300

    Added station controller, crud operations

--- Malacar/Startup.cs
+++ Malacar/Startup.cs
@@ -24,6 +24,7 @@ namespace Malacar
             services.AddControllersWithViews();
             services.AddDbContext<CarContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
