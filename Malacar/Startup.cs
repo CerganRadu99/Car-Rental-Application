f36cfa3c0e983d0bc5e304878bcd74f259b13314
CiprianP99 <57409814+CiprianP99@users.noreply.github.com>
2021-04-12 16:07:31 +0300

    Added frontend for index page

--- Malacar/Startup.cs
+++ Malacar/Startup.cs
@@ -24,7 +24,9 @@ namespace Malacar
             services.AddControllersWithViews();
             services.AddDbContext<CarContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
-           
+            services.AddRazorPages()
+                .AddRazorRuntimeCompilation();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
